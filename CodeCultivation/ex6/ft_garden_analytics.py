3efff4e23618bdc18074d46288926cf514fa04dc
Yazeed Barakat <ybarakat@c1r5s2.irbid.42.school>
2026-02-15 16:59:40 +0300

    l

--- CodeCultivation/ex6/ft_garden_analytics.py
+++ CodeCultivation/ex6/ft_garden_analytics.py
@@ -1,7 +1,39 @@
+class GardenManager:
+    users = []
+    def __init__(self, user_name: str):
+        users += [user_name]
+    class GardenStats:
+        def print_report(self):
+             
+
 class Plant:
-    def __init__(self, name, height, age):
+    def __init__(self, name: str, height: int):
         self.name = name
         self.height = height
-        self.age = age
 
-    
+    def grow(self, plant_type: str, growth: int) -> None:
+        self.height += growth
+        print(f"{self.name} {plant_type} grew {growth}cm")
+
+    def get_score(self) -> int:
+        return self.height
+class FloweringPlant(Plant):
+    def __init__(self, name: str, height: int, color: str):
+        super().__init__(name, height)
+        self.color = color
+        self.bloom = False
+
+    def blooming(self):
+        self.bloom = True
+
+
+class PrizeFlower(FloweringPlant):
+    def __init__(self, name: str, height: int, color: str, points: int)
+        super().__init__(name, height, color)
+        self.points = points
+
+
+if __name__ = "__main__":
+    print("=== Garden Management System Demo ===")
+
+    print("
