0b87b4e34037c933f291db368b9084966dd52fe9
Yazeed <yazeedbarakat731@gmail.com>
2026-06-16 11:26:40 +0300

    s

--- Func_Mage/ex4/decorator_mastery.py
+++ Func_Mage/ex4/decorator_mastery.py
@@ -1,23 +1,56 @@
 import functools
+import time
+from collections.abc import Callable
+
 
 def spell_timer(func: Callable) -> Callable:
     @functools.wraps(func)
     def wrapper(*args, **kwargs) -> Callable:
-        print("Casting", func.__name__)
+        print(f"Casting {func.__name__}...")
         t1 = time.time()
         fun = func(*args, **kwargs)
         t2 = time.time()
-        print(f"Spell completed in {t1 - t2} seconds")
+        print(f"Spell completed in {t2 - t1:.3f} seconds")
+        return fun
     return wrapper
+
+
+def power_validator(min_power: int) -> Callable:
+    def decorator(func: Callable) -> Callable:
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs) -> Callable | str:
+            if args[0] >= min_power:
+                return func(*args, **kwargs)
+            return "Insufficient power for this spell"
+        return wrapper
+    return decorator
+
+
+def retry_spell(max_attempts: int) -> Callable:
+    def decorator(func: Callable) -> Callable:
+        def wrapper(*args, **kwargs) -> str:
+            for i in range(1, max_attempts):
+                try:
+                    return func(*args, **kwargs)
+                except Exception:
+                    print(f"Spell failed, retrying... ({i}/{max_attempts})")
+            return f"Spell casting failed after {max_attempts} attempts"
+        return wrapper
+    return decorator
 
 
 @spell_timer
 def fireball() -> str:
-    return "hit using fireball"
+    return "Fireball cast!"
+
+
+def lightning(power: int) -> str:
+    return f"Successfully cast Lightning with {power} power"
 
 
 def main() -> None:
-    
+    print("Testing retrying spell...")
+    print("Result:", fireball())
 
 
 if __name__ == "__main__":
